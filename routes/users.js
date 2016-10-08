1f54c1a587dc2563f66a702c1cb963e6e3e66526
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-09 07:10:31 +0900

    Get implemented!

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(req.query.userID + 'sdflkjasdf');
 });
 
 module.exports = router;
