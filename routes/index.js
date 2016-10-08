7a590c51fca46fa6c50b7d8ecee68fe5b9ee4cfa
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-08 08:35:38 +0000

    Show data

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,31 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient,
   assert = require('assert');
 
+var url = 'mongodb://54.186.195.78:27017/trauma';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var content = '';
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    var collection = db.collection('documents');
+
+    content = collection.find({}).toArray(function(err, docs){
+      assert.equal(null, err);
+      content = JSON.stringify(docs);
+
+			console.log('Sended.');
+			db.close();
+
+			res.render('index', { title: 'Express', content: content });
+    });
+  });
 });
 
 router.post('/', function(req, res, next) {
-  var url = 'mongodb://54.186.195.78:27017/trauma';
-
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
